ac6d0e93933fd4c335cd16f179fcdda3d7298ba3
Ahmed Nabil <a.ahmed.nabil90@gmail.com>
2020-01-04 20:48:02 +0200

    remote repo unit test

--- app/src/test/java/com/nabil/postsapp/local/LocalRepoTest.kt
+++ app/src/test/java/com/nabil/postsapp/local/LocalRepoTest.kt
@@ -5,7 +5,6 @@ import com.nabil.postsapp.posts.model.local.PostsDAO
 import com.nabil.postsapp.posts.model.local.PostsDatabase
 import com.nabil.postsapp.posts.model.local.PostsLocalRepo
 import com.nabil.postsapp.posts.model.pojo.Post
-import io.reactivex.Single
 import org.junit.Before
 import org.junit.Test
 import org.mockito.Mockito.*
@@ -28,15 +27,13 @@ class LocalRepoTest {
 
     @Test
     fun getPosts_Room_isCalled() {
-        `when`(dao.getPosts()).thenReturn(Single.just(arrayListOf()))
-        localRepo.getPosts(false)
+        localRepo.getPosts(false) //internet avaliable boolean makes no diff
         verify(dao, times(1)).getPosts()
     }
 
     @Test
     fun insertPost_Room_isCalled() {
         val post = Post(1, 3, "title", "body")
-        `when`(dao.addPost(post)).thenReturn(1L)
         localRepo.addPost(post)
         verify(dao, times(1)).addPost(post)
         verify(dao, NoMoreInteractions()).addPost(post)
@@ -48,7 +45,6 @@ class LocalRepoTest {
             Post(1, 5, "title1", "body1"),
             Post(2, 5, "title2", "body2")
         )
-        `when`(dao.addAllPosts(arrayListOfPosts)).thenReturn(arrayListOf(0, 1))
         localRepo.addAllPosts(arrayListOfPosts)
         verify(dao, times(1)).addAllPosts(arrayListOfPosts)
         verify(dao, NoMoreInteractions()).addAllPosts(arrayListOfPosts)
@@ -57,7 +53,6 @@ class LocalRepoTest {
     @Test
     fun editPost_Room_isCalled() {
         val post = Post(1, 3, "title", "body")
-        `when`(dao.editPost(post)).thenReturn(1)
         localRepo.editPost(post)
         verify(dao, times(1)).editPost(post)
         verify(dao, NoMoreInteractions()).editPost(post)
@@ -66,7 +61,6 @@ class LocalRepoTest {
     @Test
     fun deletePost_Room_isCalled() {
         val post = Post(1, 3, "title", "body")
-        `when`(dao.deletePost(post)).thenReturn(1)
         localRepo.deletePost(post)
         verify(dao, times(1)).deletePost(post)
         verify(dao, NoMoreInteractions()).deletePost(post)
---
+++ app/src/test/java/com/nabil/postsapp/remote/RemoteRepoTest.kt
@@ -0,0 +1,54 @@
+package com.nabil.postsapp.remote
+
+import com.nabil.postsapp.posts.model.Repository
+import com.nabil.postsapp.posts.model.pojo.Post
+import com.nabil.postsapp.posts.model.pojo.Response
+import com.nabil.postsapp.posts.model.remote.PostsNetwork
+import com.nabil.postsapp.posts.model.remote.PostsRemoteRepo
+import io.reactivex.Observable
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mockito.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class RemoteRepoTest {
+
+    private var postsNetwork = mock(PostsNetwork::class.java)
+    private lateinit var remoteRepo: Repository<Observable<Response>, Observable<Response>>
+    private lateinit var post: Post
+
+    @Before
+    fun setUp() {
+        remoteRepo = PostsRemoteRepo(postsNetwork)
+        post = mock(Post::class.java)
+    }
+
+    @Test
+    fun testRemoteGetPosts() {
+        remoteRepo.getPosts(true)  //internet avaliable boolean makes no diff
+        verify(postsNetwork, times(1)).getPosts()
+    }
+
+    @Test
+    fun testRemoteAddPost() {
+        remoteRepo.addPost(post)
+        verify(postsNetwork, times(1)).addPost(post)
+    }
+
+    @Test
+    fun testRemoteEditPost() {
+        remoteRepo.editPost(post)
+        verify(postsNetwork, times(1)).editPost(post.id)
+    }
+
+    @Test
+    fun testRemoteDeletePost() {
+        remoteRepo.deletePost(post)
+        verify(postsNetwork, times(1)).deletePost(post.id)
+    }
+
+}
